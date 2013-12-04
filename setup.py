657957994ba7de40a0642f7a364e50daa35235cc
Simon Walker <s.r.walker101@googlemail.com>
2013-12-04 23:19:02 +0000

    Revert "long description from generated html"
    This does not work on pypi
    
    This reverts commit 166709fb4755b8571e19fee87cc151b6aecd8bc4.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         url='https://github.com/mindriot101/matplotlib-pngrenderer',
         packages=['pngrenderer',],
         install_requires=['matplotlib'],
-        long_description=read('doc/description.html'),
+        long_description=read('README.markdown'),
         tests_require=['pytest'],
         cmdclass={'test': PyTest},
         )
