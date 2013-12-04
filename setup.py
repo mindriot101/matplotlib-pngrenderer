98ee0a07d1d26c791a4a03190ddeb849bcffaf8e
Simon Walker <s.r.walker101@googlemail.com>
2013-12-04 22:45:57 +0000

    Adds ability to run tests

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 import os
+import sys
 
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(
         name='pngrenderer',
@@ -15,4 +28,6 @@ setup(
         packages=['pngrenderer',],
         install_requires=['matplotlib'],
         long_description=read('README.markdown'),
+        tests_require=['pytest'],
+        cmdclass={'test': PyTest},
         )
